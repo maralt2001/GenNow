9f77ff837755e06a03466ca354715df8ea35377d
Markus Altendorfer <markus.altendorfer@hotmail.de>
2022-02-07 13:58:17 +0100

    create node express server

--- server/src/app.js
+++ server/src/app.js
@@ -1,2 +1,20 @@
 
-console.log("hallo welt");
+const express = require('express')
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('../client/build'));
+
+app.get('/', (req, res) => {
+    let resolevePath = path.resolve('../client/build');
+    res.sendFile(resolevePath, 'index.html')
+    
+});
+
+const PORT = process.env.PORT || 5000;
+
+console.log('server started on port:',PORT);
+app.listen(PORT);
+
+
